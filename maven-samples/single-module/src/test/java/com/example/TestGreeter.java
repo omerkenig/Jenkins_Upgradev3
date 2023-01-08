5a996eea4aad811cfd5c3895d3a12138326e12a9
omerkenig <omerkenig@gmail.com>
2023-01-08 08:38:13 +0200

    1

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,7 +44,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
    @Test
-  public void greetShouldIncludeGreetingMessage1() {
+  public void greetShouldIncludeGreetingMessage2() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
